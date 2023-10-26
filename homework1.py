e44d82df5f01ecac5e80f033a13d70ad3a39d1dd
Miekhova <protaselena5@gmail.com>
2023-10-26 12:19:17 +0300

    homework.py

---
+++ homework1.py
@@ -0,0 +1,14 @@
+Spysok = [Olena, Anna, Olha, Inna]
+    print(Spysok)
+    print(sum(Spysok))
+
+Spysok2 = [1, 2, 1, 3, 4, 1]
+    print(set(Spysok2))
+
+    Slovnyk = { 'name':Olena
+                'age': 34
+                'salary': 1000}
+    print(Slovnyk)
+    Slovnyk ['salary']*=1.5
+    print(Slovnyk)
+
